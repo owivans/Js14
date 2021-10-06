0d0e9465343848eca0f893a86ed3fb7a4ee14876
owivans <ivan.diaz@nowports.com>
2021-10-05 21:07:18 -0500

    add new class

---
+++ module-js/04-ciclos/js/script.js
@@ -0,0 +1,77 @@
+
+// const nombres = [
+//     { nombre: 'freddy',}, 
+//     { nombre: 'michael'} , 
+//     { nombre: 'rafa'}
+// ];
+
+// const person = {
+//     nombre: 'freddy',
+//     apellido: 'Krueger',
+//     edad: 40,
+// };
+
+// console.log(nombres[1])
+// console.log(nombres[0])
+
+// for (let i = 0; i < nombres.length; i++) {
+//     console.log(nombres[i].nombre)
+//     // const element = array[index];
+// }
+
+let vuelta = 0;
+
+while(vuelta < 10) {
+    console.log(vuelta, 'vuelta')
+    if(vuelta % 2 === 0) {
+        console.log(`${vuelta} es par`)
+    } else {
+        console.log(`${vuelta} es impar`)
+    }
+    // console.log('se cumpli condicion con while')
+    vuelta++
+}
+
+// for ( killer in person ) {
+//     console.log(killer)
+//     console.log( person[killer] )
+// }
+
+
+
+/*
+    Ejercicio 1:
+    Pedir al usuario usuario un numero entre 1 y 10
+    Imprimir la tabla de multiplicar de ese número
+    p.ej.
+    3 x 1 = 3
+    3 x 2 = 6
+*/
+
+/*
+Ejercicio 2:
+Pedir al usuario un numero entre 10 y 100
+Imprimir todos los numeros pares que existen entre 1 y ese numero
+*/
+
+/*
+Ejercicio 3:
+Imprimir en consola la suma total de todos los digitos de una cantidad p.ej. "1234" -> 10
+*/
+
+/*
+Ejercicio 4:
+Imprimir en consola el siguiente patrón
+
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+**********
+
+*/
