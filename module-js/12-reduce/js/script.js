ad7a0c762bdb4a1ba0b9bd740e047ddca9f36b40
owivans <ivan.diaz@nowports.com>
2021-10-14 20:47:36 -0500

    new seassion

---
+++ module-js/12-reduce/js/script.js
@@ -0,0 +1,58 @@
+let arrayPersons = [
+    {
+        name: "Phibee", age: 25, voted: true, gender: "mujer"
+    },
+    {
+        name: "Katrinka", age: 26, voted: false, gender: "mujer"
+    },
+    {
+        name: "Bruno", age: 48, voted: false, gender: "hombre"
+    },
+    {
+        name: "Jon", age: 22, voted: true, gender: "hombre"
+    },
+    {
+        name: "Brian", age: 20, voted: false, gender: "hombre"
+    },
+    {
+        name: "Eugene", age: 38, voted: true, gender: "hombre"
+    },
+    {
+        name: "Big Bob", age: 29, voted: true, gender: "hombre"
+    },
+    {
+        name: "Ximena", age: 40, voted: false, gender: "mujer"
+    },
+    {
+        name: "Paulet", age: 37, voted: false, gender: "mujer"
+    },
+    {
+        name: "Harold", age: 45, voted: true, gender: "hombre"
+    },
+    {
+        name: "Gerald", age: 22, voted: false, gender: "hombre"
+    },
+    {
+        name: "Arnold", age: 35, voted: true, gender: "hombre"
+    },
+    {
+        name: "Gelga", age: 28, voted: true, gender: "mujer"
+    },
+    {
+        name: "Lila", age: 18, voted: true, gender: "mujer"
+    },
+    {
+        name: "Rhonda", age: 18, voted: true, gender: "mujer"
+    },
+]
+
+/*
+Usar reduce para obtener la siguiente informacion
+- Un array con aquellas personas que se vacunaron
+- la edad promedio de todos los vacunados
+- un array con aquellas personas que se vacunaron y que son menores a 30 años
+- la edad promedio de los no vacunados
+- el porcentaje de vacunados vs no vacunados
+- el procentaje de mujeres que se vacunaron
+- el porcentaje de los hombres que vacunarion
+*/
