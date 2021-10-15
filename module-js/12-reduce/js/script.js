ca72990ccc8c3b884e165cd68c7dd8fcec5a5095
owivans <ivan.diaz@nowports.com>
2021-10-14 22:11:06 -0500

    new seassion

--- module-js/12-reduce/js/script.js
+++ module-js/12-reduce/js/script.js
@@ -56,3 +56,45 @@ Usar reduce para obtener la siguiente informacion
 - el procentaje de mujeres que se vacunaron
 - el porcentaje de los hombres que vacunarion
 */
+
+
+
+
+
+
+
+
+//   1-recoger dos dias de tareas
+//   2- convertur las duraciones de las tareas en horas, en lugar de minutos
+//   3-Filtarar todo lo que tomo dos horas o mas
+//   4-Sumar todo
+//   5-Multiplica el resultado por una tarifa de hora para facturacion por dia
+//   6- produce una cantidad formateada en dolares
+//   7-encadenar todos los metodos
+
+
+  const monday = [
+    {
+        'name'     : 'Write a tutorial',
+        'duration' : 180
+    },
+    {
+        'name'     : 'Some web development',
+        'duration' : 120
+    }
+];
+
+const tuesday = [
+    {
+        'name'     : 'Keep writing that tutorial',
+        'duration' : 240
+    },
+    {
+        'name'     : 'Some more web development',
+        'duration' : 180
+    },
+    {
+        'name'     : 'A whole lot of nothing',
+        'duration'  : 240
+    }
+];
