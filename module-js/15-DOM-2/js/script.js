136a4a47c3a49c425acd8cb4f308682e5efa1835
owivans <ivan.diaz@nowports.com>
2021-10-27 21:17:17 -0500

    add new session

--- module-js/15-DOM-2/js/script.js
+++ module-js/15-DOM-2/js/script.js
@@ -1,84 +1,115 @@
-// [
-//     {
-//         name:'ivan',
-//         lastName: 'Diaz',
-//     }
-// ]
+// --------------------
+const getKoders = () => {
+  const xhr = new XMLHttpRequest();
+  const URL = "https://js-14va-default-rtdb.firebaseio.com/marco/.json";
 
-// -crear una function que me genere una persona
-// asignarle la funcion al boton para recolectar los datos el dinput
-// obtener el boton de guardar
-// asignarle un listener
-// detectar el evento click
-// con el callback obtener los datos de los inputs
-//con los datos obtenidos armar el objeto y almacenarlo en un arreglo 
-const personList = [
-    {
-        name:'penny',
-        lastName: 'wise',
-    },
-    {
-        name:'jason',
-        lastName: 'vorhees',
+  xhr.addEventListener("readystatechange", () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const users = JSON.parse(xhr.responseText);
+
+      createRow(users);
+    } else {
+      console.log("hubo un error");
     }
-];
+  });
+  xhr.open("GET", URL, true);
+  console.log(xhr);
+  xhr.send();
+};
+
+const postKoder = () => {
+  const xhr = new XMLHttpRequest();
+  const URL = "https://js-14va-default-rtdb.firebaseio.com/.json";
+  xhr.addEventListener("readystatechange", () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+      }
+    } else {
+      console.log(xhr.readyState);
+    }
+  });
+  xhr.open("POST", URL, true);
+  xhr.send(JSON.stringify({ name: "ivan", lastName: "diaz" }));
+};
+
+const deleleUser = userId => {
+  const xhr = new XMLHttpRequest();
+  const URL = `https://js-14va-default-rtdb.firebaseio.com/marco/${userId}/.json`;
+  xhr.addEventListener("readystatechange", () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+      }
+    } else {
+      console.log(xhr.readyState);
+    }
+  });
+  xhr.open("DELETE", URL, true);
+  xhr.send();
+};
+
 const button = document.getElementById("savePerson");
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll("input");
 const tBody = document.getElementById("list");
-console.log(tBody.firstElementChild)
 
-console.log(document.querySelector('tbody').closest('table'))
 const createPerson = () => {
-    const person = {
-        name:'',
-        lastName:''
-    }
-    inputs.forEach((input) => {
-        person[input.name] = input.value
-    });
-    personList.push(person);
+  const person = {
+    name: "",
+    lastName: ""
+  };
+  inputs.forEach(input => {
+    person[input.name] = input.value;
+  });
+  personList.push(person);
 };
 
-const createRow = () => {
+const createRow = userList => {
+    const newPersonas = [];
     while (tBody.lastElementChild) {
-        tBody.removeChild(tBody.lastElementChild)
+        tBody.removeChild(tBody.lastElementChild);
     }
-    
-    personList.forEach((person, index) => {
-        const row = document.createElement('tr');
-        const name = document.createElement('td');
-        const lastName = document.createElement('td');
-        const removeButton = document.createElement('button');
-        removeButton.setAttribute('data-person', index)
 
-        name.textContent = person.name;
-        lastName.textContent = person.lastName;
-        removeButton.textContent = 'Delete';
-        row.appendChild(name);
-        row.appendChild(lastName);
-        row.appendChild(removeButton);
-        tBody.appendChild(row);
+    for (let key in userList) {
+        const object = {
+        id: key,
+        ...userList[key]
+        };
+        newPersonas.push(object);
+    }
 
-        removeButton.addEventListener('click', (event) => {
-            console.log(event)
-            removeButton.closest('tr').remove();
-            console.log(personList)
-            const personIndex = event.target.dataset.person;
-            console.log(personIndex)
-            personList.splice(personIndex, 1);
-            console.log(personList, 'personList')
-        });
+  console.log(newPersonas);
+  newPersonas.forEach((person, index) => {
+    console.log(person);
+    const row = document.createElement("tr");
+    const name = document.createElement("td");
+    const lastName = document.createElement("td");
+    const removeButton = document.createElement("button");
+    removeButton.setAttribute("data-person", person.id);
+
+    name.textContent = person.name;
+    lastName.textContent = person.lastName;
+    removeButton.textContent = "Delete";
+    row.appendChild(name);
+    row.appendChild(lastName);
+    row.appendChild(removeButton);
+    tBody.appendChild(row);
+
+    removeButton.addEventListener("click", event => {
+      console.log(event);
+      removeButton.closest("tr").remove();
+      const personId = event.target.dataset.person;
+      deleleUser(personId);
     });
+  });
 };
 
-
-button.addEventListener('click', (e)=> {
-    console.log(e)
-    createPerson();
-    createRow();
-    console.log(inputs, 'inputs')
+button.addEventListener("click", e => {
+  console.log(e);
+  createPerson();
+  createRow();
+  console.log(inputs, "inputs");
 });
 
-createRow()
-
-
+createRow();
+getKoders();
